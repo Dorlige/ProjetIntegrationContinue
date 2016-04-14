af959946405b5201aa7f888b5411cfed20382dc4
Dorlige <saindy.nkadi@yahoo.fr>
2016-04-14 14:35:33 +0200

    Update monscript.sh

--- monscript.sh
+++ monscript.sh
@@ -24,10 +24,10 @@ cp  -Rf $HOME/html/faq.html ./index.html
  #add, commit and push files
  git add -f .
  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages "
- git push -fq origin gh-pages > /dev/null
+# git push -fq origin gh-pages > /dev/null
  #git push -fq origin master > /dev/null
 
- #git push -f -q https://${GH_TOKEN}@github.com/${GH_REF}   master > /dev/null
+ git push -f -q https://${GH_TOKEN}@github.com/${GH_REF}   master > /dev/null
 
 
 
