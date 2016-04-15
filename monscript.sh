d830ca22315dfb88cae1c819c6cfe090144745eb
Dorlige <saindy.nkadi@yahoo.fr>
2016-04-15 19:52:59 +0200

    Update monscript.sh

--- monscript.sh
+++ monscript.sh
@@ -8,11 +8,11 @@ cd $HOME
 git config --global user.email "saindy.nkadi@yahoo.fr"
 git config --global user.name "Dorlige"
 
-#git clone --quiet https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git gh-pages > /dev/null
+git clone --quiet https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git gh-pages > /dev/null
 
 
-cd master
-git checkout master
+cd gh-pages
+git checkout gh-pages
 
 
  #git remote rm origin
@@ -28,7 +28,7 @@ git checkout master
  
  #git checkout gh-pages
  
-  git push -fq origin master
+  git push -fq origin gh-pages
   #git push -fq https://${GH_TOKEN}@github.com/Dorlige/ProjetIntegrationContinue.git master > /dev/null
  echo -e "Deploy completed\n"
  #fi
